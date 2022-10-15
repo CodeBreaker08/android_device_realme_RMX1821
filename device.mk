d1c51ec31cc0aa7ba0ef2cda33362462a02aa5a1
Arrowsploit <arrowsploit@pm.me>
2022-10-15 23:14:39 +0530

    RMX1821: Import input configs
     * From : * From: full_oppo6771-user 10 QP1A.190711.020 08805472cb0b87b3 release-keys

--- device.mk
+++ device.mk
@@ -7,6 +7,12 @@
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/realme/RMX1821/RMX1821-vendor.mk)
 
+# Input Configs
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PATH)/configs/input/idc/AVRCP.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/AVRCP.idc \
+	$(DEVICE_PATH)/configs/input/keylayout/ACCDET.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/ACCDET.kl \
+	$(DEVICE_PATH)/configs/input/keylayout/AVRCP.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/AVRCP.kl
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
 	$(DEVICE_PATH)
