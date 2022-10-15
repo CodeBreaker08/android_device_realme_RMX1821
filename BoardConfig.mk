ec2093a8451df575b4e6823b020b5f157ab289b3
Arrowsploit <arrowsploit@pm.me>
2022-10-15 20:29:24 +0530

    RMX1821: Define device architecture

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,3 +5,22 @@
 #
 
 DEVICE_PATH := device/realme/RMX1821
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_CPU_ABI_LIST := arm64-v8a,armeabi-v7a,armeabi
+TARGET_CPU_ABI_LIST_64_BIT := arm64-v8a
+TARGET_CPU_ABI_LIST_32_BIT := armeabi-v7a,armeabi
+
+TARGET_USES_64_BIT_BINDER := true
