626b91c2a07fd1b521a0cd4e18359a9a8c48fe76
Arrowsploit <arrowsploit@pm.me>
2022-10-16 19:15:57 +0530

    RMX1821: Copy over stock supplicant configs
    * From: full_oppo6771-user 10 QP1A.190711.020 08805472cb0b87b3 release-keys

--- device.mk
+++ device.mk
@@ -61,3 +61,9 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
 	$(DEVICE_PATH)
+
+# WiFi
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+	$(DEVICE_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+	$(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
