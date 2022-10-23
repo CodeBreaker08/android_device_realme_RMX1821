dfeca1a931eb6cafec684a15c9bf08f77fdfdd13
Debayan <kardebayan3@gmail.com>
2022-10-23 18:05:24 +0530

    RMX1821: device: Build libvulkan

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Display
+PRODUCT_PACKAGES += \
+	libvulkan
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
 	android.hardware.gatekeeper@1.0-service \
