acbda3957441a326a75969970ee6910c29f40ab9
Arrowsploit <arrowsploit@pm.me>
2022-11-29 12:40:47 +0530

    RMX1821: device: Build libminijail

--- device.mk
+++ device.mk
@@ -135,6 +135,9 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+PRODUCT_PACKAGES += \
+	libminijail
+
 # Neural Networks
 PRODUCT_PACKAGES += \
 	android.hardware.neuralnetworks@1.0.vendor \
