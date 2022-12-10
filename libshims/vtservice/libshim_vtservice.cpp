a6499408efc26e8f895ff17e81744f4e222314a2
SamarV-121 <samarvispute121@pm.me>
2022-12-10 11:02:48 +0530

    RMX1821: Shim another missing symbol for vtservice
    * Fixes :
    - F linker  : CANNOT LINK EXECUTABLE "/system/bin/vtservice": cannot locate symbol "_ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf" referenced by "/system/lib/libsink.so"...
    
    Signed-off-by: Arrowsploit <arrowsploit@pm.me>

--- libshims/vtservice/libshim_vtservice.cpp
+++ libshims/vtservice/libshim_vtservice.cpp
@@ -1,3 +1,4 @@
 extern "C" {
     void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb() {}
+    void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf() {}
 }
