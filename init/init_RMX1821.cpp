80617fd06d8fa3c3118fefc3bbfd46a4c8e08b9e
SamarV-121 <samarvispute121@pm.me>
2023-05-06 14:32:53 +0530

    RMX1821: init: Refactor code

--- init/init_RMX1821.cpp
+++ init/init_RMX1821.cpp
@@ -1,84 +1,105 @@
-/*
-   Copyright (c) 2023, The LineageOS Project
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+//
+// Copyright (C) 2023 The LineageOS Project
+//
+// SPDX-License-Identifier: Apache-2.0
+//
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-
-#include <android-base/file.h>
+#include <fstream>
+#include <tuple>
 #include <android-base/logging.h>
-#include <android-base/properties.h>
-#include <android-base/strings.h>
-
-#include <sys/sysinfo.h>
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
-#include "property_service.h"
 
+#define PROC_OPERATOR "/proc/oppoVersion/operatorName"
+
+#include <sys/sysinfo.h>
+
+#include <android-base/file.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
+#include "property_service.h"
 using android::base::ReadFileToString;
-using android::base::Trim;
 using std::string;
 
-std::vector<std::string> ro_props_default_source_order = {
-    "",
-    "odm.",
-    "product.",
-    "system.",
-    "system_ext.",
-    "vendor.",
-};
+void property_override(std::string prop, std::string value, bool add = true) {
+    auto pi = (prop_info *)__system_property_find(prop.c_str());
 
-void property_override(char const prop[], char const value[], bool add = true) {
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else if (add)
-        __system_property_add(prop, strlen(prop), value, strlen(value));
+    if (pi != nullptr) {
+        __system_property_update(pi, value.c_str(), value.size());
+    } else if (add) {
+        __system_property_add(prop.c_str(), prop.size(), value.c_str(),
+                              value.size());
+    }
 }
 
-void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = true) {
-    string prop_name;
+void set_ro_build_prop(const std::string &prop, const std::string &value,
+                       bool product = true) {
+    std::string prop_name;
+    std::string prop_types[] = {
+        "",
+        "bootimage.",
+        "odm.",
+        "odm_dlkm.",
+        "product.",
+        "system.",
+        "system_ext.",
+        "vendor.",
+        "vendor_dlkm.",
+    };
 
-    for (const auto &source : ro_props_default_source_order) {
-        if (product)
+    for (const std::string &source : prop_types) {
+        if (product) {
             prop_name = "ro.product." + source + prop;
-        else
+        } else {
             prop_name = "ro." + source + "build." + prop;
+        }
 
-        property_override(prop_name.c_str(), value.c_str());
+        property_override(prop_name, value, false);
     }
+}
+
+std::tuple<std::string, std::string> get_device() {
+    std::string device, model, line;
+    std::ifstream operator_file(PROC_OPERATOR);
+    operator_file.good();
+    getline(operator_file, line);
+
+    int operatorName = stoi(line);
+
+    switch (operatorName) {
+    /*
+     * Variants of Realme 3 and Realme 3i can be distinguished
+     * by the value of /proc/oppoVersion/operatorName
+     *
+     * 131 -> Realme 3  RMX1821
+     * 133 -> Realme 3  RMX1825
+     * 137 -> Realme 3i RMX1827
+     * ?   -> Realme 3  RMX1822
+     * ?   -> Realme 3i RMX1823
+     * TODO: Find the values for RMX1822 and RMX1823
+     */
+        // realme 3
+        case 130:
+        case 131:
+            device = "RMX1821";
+            model = "RMX1821";
+            break;
+        case 133:
+            device = "RMX1825";
+            model = "RMX1825";
+            break;
+        // realme 3i
+        case 137:
+            device = "RMX1827";
+            model = "RMX1827";
+            break;
+        default:
+            LOG(ERROR) << "Unable to read operator from " << PROC_OPERATOR;
+    }
+    return {device, model};
 }
 
 void init_fp_properties()
@@ -104,41 +125,8 @@ void set_avoid_gfxaccel_config() {
 }
 
 void vendor_load_properties() {
-    char const *operator_file = "/proc/oppoVersion/operatorName";
-    string operator_name;
-    string device;
-    string model;
-    /*
-     * Variants of Realme 3 and Realme 3i can be distinguished
-     * by the value of /proc/oppoVersion/operatorName
-     *
-     * 131 -> Realme 3  RMX1821
-     * 133 -> Realme 3  RMX1825
-     * 137 -> Realme 3i RMX1827
-     * ?   -> Realme 3  RMX1822
-     * ?   -> Realme 3i RMX1823
-     * TODO: Find the values for RMX1822 and RMX1823
-     */
-    if (ReadFileToString(operator_file, &operator_name)) {
-        // Realme 3
-        if (Trim(operator_name) == "130") {
-            device = "RMX1821";
-            model = "RMX1821";
-        }
-        // Realme 3
-        else if (Trim(operator_name) == "133") {
-            device = "RMX1825";
-            model = "RMX1825";
-        }
-        // Realme 3i
-        else if (Trim(operator_name) == "137") {
-            device = "RMX1827";
-            model = "RMX1827";
-        }
-    }
-    else {
-        LOG(ERROR) << "Unable to read operator from " << operator_file;
-    }
+    auto [device, model] = get_device();
+
     set_ro_build_prop("device", device);
     set_ro_build_prop("model", model);
     set_ro_build_prop("name", model);
