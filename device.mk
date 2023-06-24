c13007f5adc0d560a2dac8a45a9f4b4e9ffda8b7
krasCGQ <kras@raphielgang.org>
2023-06-24 13:36:51 +0000

    RMX1821: device: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.

--- device.mk
+++ device.mk
@@ -127,6 +127,10 @@ PRODUCT_PACKAGES += \
 	libhwbinder \
 	libhwbinder.vendor \
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Input Configs
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/input/idc/AVRCP.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/AVRCP.idc \
