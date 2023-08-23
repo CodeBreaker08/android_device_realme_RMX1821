ccb347afae66af6a68bde801e2f9d6a62049257a
Code Breaker <141673924+CodeBreaker08@users.noreply.github.com>
2023-08-24 00:52:53 +0530

    Pixel Experience Fly Up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,3 +8,8 @@ DEVICE_PATH := device/realme/RMX1821
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_RMX1821.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_RMX1821-user \
+    aosp_RMX1821-userdebug \
+    aosp_RMX1821-eng
